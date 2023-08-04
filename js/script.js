228edb5e42c2290e48d3e3a33887860cdb51cd5a
Hamed <hamedpyn@gmail.com>
2023-08-04 11:53:26 +0430

    adding an alert and a condition

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ let
     searchBtn = document.querySelector('.search button'),
     searchIn = document.querySelector('.search input'),
     weather = document.querySelector('.weather'),
+    weatherStatus = document.querySelector('.description'),
     citiesData = {
-        tehran: { city: 'Tehran', temp: '33', humidity: '20%', wind: '2.6' },
+        tehran: { city: 'Tehran', temp: '33', weather: 'Sunny', humidity: '20%', wind: '2.6' },
         shiraz: { city: 'Shiraz', temp: '3', weather: 'Snow', humidity: '60%', wind: '8.9' },
         bushehr: { city: 'Bushehr', temp: '9', weather: 'Rainy', humidity: '40%', wind: '9.2' },
         berlin: { city: 'Berlin', temp: '15', weather: 'Tornado', humidity: '12%', wind: '16.5' },
@@ -29,12 +30,18 @@ let
 
 searchBtn.addEventListener('click', () => {
     let searchInputValue = searchIn.value
-    searchInputValue.toLowerCase()
-    let mainData =  citiesData[searchInputValue]
-    city.innerHTML = `Weather in ${mainData.city}`
-    temp.innerHTML = `${mainData.temp}°C`
-    humidity.innerHTML = `Humidity: ${mainData.humidity}`
-    wind.innerHTML = `Wind speed: ${mainData.wind} km/h`
-    weather.classList.remove('loading')
+    let lowerValue = searchInputValue.toLowerCase()
+    let mainData = citiesData[lowerValue]
+
+    if (mainData) {
+        city.innerHTML = `Weather in ${mainData.city}`
+        temp.innerHTML = `${mainData.temp}°C`
+        weatherStatus.innerHTML = `${mainData.weather}`
+        humidity.innerHTML = `Humidity: ${mainData.humidity}`
+        wind.innerHTML = `Wind speed: ${mainData.wind} km/h`
+        weather.classList.remove('loading')
+    } else {
+        alert('این صفحه یک تست است و از شهر های زیادی استفاده نکردم . لطفا شهر دیگری را استفاده کنید\n - This page is an exercise and I did not use many cities.Please use another city')
+    }
 })
 
