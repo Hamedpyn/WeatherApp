42dbba600faa8f3c1c9bd8687dfbcb069f5c19c3
Hamed <hamedpyn@gmail.com>
2023-08-17 19:38:53 +0430

    if the city wasn't exist in our database :alert

--- js/script.js
+++ js/script.js
@@ -27,12 +27,16 @@ searchBtn.addEventListener('click', () => {
     let searchInputValue = searchInput.value;
     // Get the weather data for the searched city
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&units=metric&appid=5986e34809c3c329649d1f7703faa5e9`).then(res => res.json()).then(data => {
-        // Display the weather data for the searched city
-        city.innerHTML = `Weather in ${data.name}`;
-        temp.innerHTML = `${data.main.temp}°C`;
-        weatherStatus.innerHTML = `${data.weather[0].main}`;
-        humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
-        wind.innerHTML = `Wind speed: ${data.wind.speed} km/h`;
-        weather.classList.remove('loading');
+        if (data.name === undefined) {
+            alert(`شهر شما وجود خارجی ندارد. \n Your city doesn's exsit.`)
+        } else {
+            // Display the weather data for the searched city
+            city.innerHTML = `Weather in ${data.name}`;
+            temp.innerHTML = `${data.main.temp}°C`;
+            weatherStatus.innerHTML = `${data.weather[0].main}`;
+            humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
+            wind.innerHTML = `Wind speed: ${data.wind.speed} km/h`;
+            weather.classList.remove('loading');
+        }
     })
 });
