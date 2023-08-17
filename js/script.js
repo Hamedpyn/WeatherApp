c04a7e66f8dece5f7f7efbb3d8f75a4ea769e8f9
Hamed <hamedpyn@gmail.com>
2023-08-17 19:27:15 +0430

    Display theWeatherData for theSearched city by API

--- js/script.js
+++ js/script.js
@@ -1,63 +1,36 @@
 ﻿// DOM Selectors
 
 // To display the city name
-let city = document.querySelector('.city');
-// To display the temperature
-let temp = document.querySelector('.temp'); 
-// To display the humidity percentage
-let humidity = document.querySelector('.humidity');
-// To display the wind speed
-let wind = document.querySelector('.wind'); 
-// To select the search button
-let searchBtn = document.querySelector('.search button'); 
-// To select the search input
-let searchInput = document.querySelector('.search input'); 
-// To select the weather container
-let weather = document.querySelector('.weather'); 
-// To display the weather status
-let weatherStatus = document.querySelector('.description'); 
-
-// Declaring an object to store weather data for different cities
-let citiesData = {
-    tehran: { city: 'Tehran', temp: '33', weather: 'Sunny', humidity: '20%', wind: '2.6' },
-    shiraz: { city: 'Shiraz', temp: '3', weather: 'Snow', humidity: '60%', wind: '8.9' },
-    bushehr: { city: 'Bushehr', temp: '9', weather: 'Rainy', humidity: '40%', wind: '9.2' },
-    berlin: { city: 'Berlin', temp: '15', weather: 'Tornado', humidity: '12%', wind: '16.5' },
-    tabriz: { city: 'Tabriz', temp: '7', weather: 'Thunder', humidity: '10%', wind: '11.3' },
-    rasht: { city: 'Rasht', temp: '-10', weather: 'Ice storm', humidity: '5%', wind: '17.1' },
-    abadan: { city: 'Abadan', temp: '9', weather: 'Lightning', humidity: '9%', wind: '12.7' },
-    paris: { city: 'Paris', temp: '11', weather: 'Wind', humidity: '20%', wind: '18.4' },
-    roma: { city: 'Roma', temp: '13', weather: 'Heat wave', humidity: '90%', wind: '3.2' },
-    milan: { city: 'Milan', temp: '4', weather: 'Sunshine', humidity: '30%', wind: '2.3' },
-    washington: { city: 'Washington DC', temp: '8', weather: 'Overcast', humidity: '60%', wind: '3.2' },
-    losAngeles: { city: 'Los Angeles', temp: '-7', weather: 'Winter storm', humidity: '10%', wind: '6.2' },
-    NewYork: { city: 'New York', temp: '6', weather: 'Rainy', humidity: '40%', wind: '6.2%' },
-    istanbul: { city: 'Istanbul', temp: '11', weather: 'Wind', humidity: '20%', wind: '18.4%' },
-    madrid: { city: 'Madrid', temp: '5', weather: 'Storm', humidity: '30%', wind: '14.7' },
-    london: { city: 'London', temp: '1', weather: 'Hail', humidity: '50%', wind: '8.6' },
-    barcelona: { city: 'Barcelona', temp: '9', weather: 'Heat wave', humidity: '90%', wind: '3.2' },
-};
+let city = document.querySelector('.city'),
+    // To display the temperature
+    temp = document.querySelector('.temp'),
+    // To display the humidity percentage
+    humidity = document.querySelector('.humidity'),
+    // To display the wind speed
+    wind = document.querySelector('.wind'),
+    // To select the search button
+    searchBtn = document.querySelector('.search button'),
+    // To select the search input
+    searchInput = document.querySelector('.search input'),
+    // To select the weather container
+    weather = document.querySelector('.weather'),
+    // To display the weather status
+    weatherStatus = document.querySelector('.description');
 
 // Add an event listener to the search button
 searchBtn.addEventListener('click', () => {
     // Get the value of the search input
     let searchInputValue = searchInput.value;
-    // Convert the search input value to lowercase
-    let lowerValue = searchInputValue.toLowerCase();
     // Get the weather data for the searched city
-    let mainData = citiesData[lowerValue];
-    // If the weather data for the searched city exists
-    if (mainData) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputValue}&units=metric&appid=5986e34809c3c329649d1f7703faa5e9`).then(res => res.json()).then(data => {
+        console.log();
         // Display the weather data for the searched city
-        city.innerHTML = `Weather in ${mainData.city}`;
-        temp.innerHTML = `${mainData.temp}°C`;
-        weatherStatus.innerHTML = `${mainData.weather}`;
-        humidity.innerHTML = `Humidity: ${mainData.humidity}`;
-        wind.innerHTML = `Wind speed: ${mainData.wind} km/h`;
+        city.innerHTML = `Weather in ${data.name}`;
+        temp.innerHTML = `${data.main.temp}°C`;
+        weatherStatus.innerHTML = `${data.weather[0].main}`;
+        humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
+        wind.innerHTML = `Wind speed: ${data.wind.speed} km/h`;
         weather.classList.remove('loading');
-    } else {
-        // Display an alert message if the weather data for the searched city does not exist
-        alert('این صفحه یک تست است و از شهر های زیادی استفاده نکردم . لطفا شهر دیگری را استفاده کنید\n - This page is an exercise and I did not use many cities.Please use another city');
-    }
-});
+    })
 
+});
